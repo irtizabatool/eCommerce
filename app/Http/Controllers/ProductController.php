7f26fd27829b1441d042b15b88f90aa4f4b8d405
irtizabatool <irtizabatool@gmail.com>
2022-09-26 22:14:11 +0530

    Php Laravel Ecommerce project: Done upto login module

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    function index()
+    {
+        return "WELCOME TO THE PRODUCT PAGE";
+    }
+}
---
+++ resources/views/header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Orders</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav d-flex">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cart(0)</a>
+              </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
